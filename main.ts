2a8db8ffd41c74b9be2ff0303f6708da3044996d
codewith-Faizi <160515803+codewith-Faizi@users.noreply.github.com>
2024-03-15 11:23:33 -0700

    Exercise 2/9
    Typescript Assignment

---
+++ main.ts
@@ -0,0 +1,35 @@
+// Q2
+//personal Message
+let personName: string ="Faizan";
+console.log(`Hello ${personName}, would you like to some python today`);
+// Q3
+//Name Cases
+let Name1:string ="Faizan";
+console.log(Name1.toLowerCase());
+console.log(Name1.toUpperCase());
+console.log(Name1.charAt(0).toUpperCase() + Name1.slice(1).toLowerCase());
+// Q4
+//Famous Quot
+console.log('Albert Einstein once said,"A person who never made a mistake never tried anything new."');
+// Q5
+//Famous Quote 2
+let famous_person:string ="Albert Einstein"
+let message: string =`${famous_person} once said,"A person who never made a mistake never tride anything new"`;
+console.log(message);
+//Q 6
+//Stripping Names
+let Name:string ="\t\n Faizan \t\n";
+console.log(Name);
+console.log(Name.trim());
+// Q7/8
+// Number Eight
+console.log(5+3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+// Q9
+// Favorite Number
+let favoriteNumber:number = 8;
+console.log(`my favorite number is ${favoriteNumber}.`);
+
+
